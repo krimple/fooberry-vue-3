bb602484260057500b7a95dd11048684cc00b904
ken Rimple <krimple@chariotsolutions.com>
2020-11-12 10:20:09 -0500

    fixup compose and use param in lambda

--- fooberry-backend/lambdas/functions/hello.py
+++ fooberry-backend/lambdas/functions/hello.py
@@ -1,4 +1,21 @@
+import os
+import boto3
+from botocore.exceptions import ClientError
+import sys
+
 def handler(event, context):
-    print (event)
-    print ('made you look')
-    return 'hi chowdah'
+
+    s3_client = boto3.client('s3', endpoint_url=os.environ.env.get('S3_ENDPOINT_URL'))
+
+    try:
+        response = s3_client.list_buckets()
+    except ClientError as e:
+        print (e.message)
+        sys.exit(0)
+
+    buckets = ''
+    for bucket in response['Buckets']:
+        print (bucket['Name'])
+        buckets = buckets + bucket['Name'] + '\n'
+
+    return buckets
